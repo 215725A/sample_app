7bb5d03a50eeb497a19040c56fcbd9170bfe0624
215725A <93891487+215725A@users.noreply.github.com>
2023-07-24 08:59:45 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 class UsersController < ApplicationController
-  def new
+  #  GET /users/:id
+  def show
+    # @user = User.first
+    @user = User.find(params[:id])
+
+    # => app/views/users/show.html.erb
   end
+
+  # GET /users/new or /signup
+  def new
+    # => app/views/users/new.html.erb
+    @user = User.new
+    # => app/views/users/new.html
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # => sucsess
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # => GET /user/:id
+    else
+      # => Failure
+      render 'new', status: :unprocessable_entity
+    end
+
+    # render 'create'
+  end
+
+  private
+    # Strong Parameters
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
