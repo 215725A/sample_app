84806d73a823318c2782cf832a47a5a402aa71f2
215725A <93891487+215725A@users.noreply.github.com>
2023-09-10 09:20:02 +0000

    Finish ch13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     # @user = User.first
     @user = User.find(params[:id])
 
-    # => app/views/users/show.html.erb
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # GET /users/new or /signup
@@ -61,15 +61,6 @@ class UsersController < ApplicationController
     end
 
     # beforeフィルタ
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
 
     # 正しいユーザーかどうか確認
     def correct_user
